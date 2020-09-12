9c62922447f477ae733d9dfa87625b99f80c334e
Andrea Sánchez <a348644@uabc.edu.mx>
2020-09-12 02:28:22 -0700

    Add second version of script Main

--- src/learning_java/Main.java
+++ src/learning_java/Main.java
@@ -5,7 +5,12 @@ import learning_java.servicios.GeneratorColor;
 public class Main {
 
 	public static void main(String[] args) {
-		String colorsList = GeneratorColor.randomColor();
-		System.out.println(colorsList);
+		int n = 6;
+		String[] colorsCodes = new String[n];
+		
+		for (int i = 0; i < n; i++) {
+			colorsCodes[i] = GeneratorColor.randomColor();
+			System.out.println(colorsCodes[i]);
+		}
 	}
 }
