8715b633275e4ceb464521b9bb06b1e76d68e259
Andrea Sánchez <a348644@uabc.edu.mx>
2020-09-12 01:13:27 -0700

    Add function to generate random colors in hex

---
+++ src/learning_java/servicios/GeneratorColor.java
@@ -0,0 +1,14 @@
+package learning_java.servicios;
+
+import java.util.Random;
+
+public class GeneratorColor{	
+	
+	public static String randomColor() {
+		Random obj = new Random();
+		int randomNum = obj.nextInt(0xffffff + 1);
+		String colorCode = String.format("#%06x", randomNum);
+		return colorCode.toString();
+	}
+	
+}
