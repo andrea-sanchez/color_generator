bb19cb08c1a70d52d644136630df5b09ec8b5f0a
Efren Cabrera <efren@cabrera.dev>
2020-09-13 02:18:53 -0700

    Create web service

---
+++ src/main/java/dev/kodice/hexcolorgenerator/web/HexColorGeneratorController.java
@@ -0,0 +1,23 @@
+package dev.kodice.hexcolorgenerator.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.kodice.hexcolorgenerator.services.RandomColorsPack;
+
+@RestController
+public class HexColorGeneratorController {
+	
+	@Autowired
+	private RandomColorsPack randomColorsPack;
+	
+	@RequestMapping("/{nColors}")
+	public List<String> hexColorGenetarot(@PathVariable int nColors) {
+		return this.randomColorsPack.colorsArray(nColors);
+	}
+
+}
