5451550b42b8318a24c781188a5b8f5033d41119
Andrea Sánchez <a348644@uabc.edu.mx>
2020-09-13 00:29:15 -0700

    Adds a descriptive comment to variable

--- src/learning_java/servicios/RandomColorGenerator.java
+++ src/learning_java/servicios/RandomColorGenerator.java
@@ -4,9 +4,10 @@ import java.util.Random;
 
 public class RandomColorGenerator{	
 	public static String randomColor() {
-		Random obj = new Random();
-		int randomNum = obj.nextInt(0xffffff + 1);
-		String colorCode = String.format("#%06x", randomNum);
+		Random color = new Random();
+		// Creates a random number. The maximum is ffffff (hex) = 16777215 (dec)
+		int randomNumber = color.nextInt(0xffffff + 1);
+		String colorCode = String.format("#%06x", randomNumber);
 		return colorCode.toString();
 	}
 }
