77a063799544c8d01d0f7cf81412684aba372609
Efren Cabrera <efren@cabrera.dev>
2020-09-14 16:20:37 -0700

    Refactor color generator
    Co-authored-by: Andrea <andrea.sanchez@kodice.dev>

--- src/main/java/dev/kodice/hexcolorgenerator/services/RandomColorsPack.java
+++ src/main/java/dev/kodice/hexcolorgenerator/services/RandomColorsPack.java
@@ -2,6 +2,8 @@ package dev.kodice.hexcolorgenerator.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,10 @@ public class RandomColorsPack {
 
 	public List<String> colorsArray(int n, int userSeed) {
 		List<String> colorsHex = new ArrayList<String>();
+		Random random = new Random();
+		random.setSeed(userSeed);
 		for (int i = 0; i < n; i++) {
-			colorsHex.add(this.randomColorGenerator.randomColorHex(userSeed + i));
+			colorsHex.add(this.randomColorGenerator.randomColorHex(Optional.of(random)));
 		}
 		return colorsHex;
 	}
